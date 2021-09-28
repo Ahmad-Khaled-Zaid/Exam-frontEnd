25608a3b63c947f4adcbc2e65df4cca5160d4389
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-09-28 11:28:26 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LoginButton from './components/LoginButton';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 import { withAuth0 } from '@auth0/auth0-react';
+import LogoutButton from './components/LogoutButton';
 
 
 class App extends React.Component {
@@ -21,9 +23,11 @@ class App extends React.Component {
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `Home` component, if they are not, render the `Login` component */}
+                <LoginButton/>
               </Route>
               <Route exact path="/favFruit">
                 {/* TODO: if the user is logged in, render the `FavFruit` component, if they are not, render the `Login` component */}
+                <LogoutButton/>
               </Route>
             </Switch>
             <Footer />
